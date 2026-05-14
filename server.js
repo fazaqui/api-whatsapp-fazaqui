8439a50099e19519e772192dd6fc13a87b32b86c
fazaqui <fazaqui@gmail.com>
2026-05-14 06:16:40 +0000

    fix: remove caminho engessado do chrome

--- server.js
+++ server.js
@@ -14,11 +14,8 @@ const client = new Client({
             '--no-sandbox',
             '--disable-setuid-sandbox',
             '--disable-dev-shm-usage',
-            '--disable-accelerated-2d-canvas',
-            '--no-first-run',
             '--no-zygote',
-            '--single-process', // <- this one doesn't works in Windows
-            '--disable-gpu'
+            '--single-process'
         ]
     }
 });
